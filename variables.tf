43262062fd6dadd2db6393e9cb78c8f31e3d3b00
Nikhil Panchal <nikhil.panchal@opstree.com>
2022-10-17 13:14:17 +0530

    updated variables

--- variables.tf
+++ variables.tf
@@ -4,82 +4,71 @@ variable "cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "name" {
-  description = "Name of the VPC to be created"
+variable "vpc_secondary_cidr" {
+  description = "The CIDR block for the VPC."
   type        = string
-  default     = "Staging-vpc"
+  default     = null
 }
 
-variable "tags" {
+variable "vpc_name" {
+  description = "Name of the VPC to be created"
+  type        = string
+  default     = "vpc"
+}
+
+variable "vpc_tags" {
   description = "Additional tags for the VPC"
   type        = map(string)
   default     = {}
 }
 
-variable "public_subnets_cidr" {
-  description = "CIDR list for public subnet"
-  type        = list(string)
-}
-
-variable "private_subnets_cidr" {
-  description = "CIDR list for private subnet"
-  type        = list(string)
-}
-
-variable "avaialability_zones" {
-  description = "List of avaialability zones"
-  type        = list(string)
-}
-
-variable "logs_bucket" {
-  description = "Name of bucket where we would be storing our logs"
+variable "igw_tags" {
+  description = "Additional tags for the VPC"
+  type        = map(string)
+  default     = {}
 }
 
 variable "logs_bucket_arn" {
   description = "ARN of bucket where we would be storing vpc our logs"
-}
-
-variable "pvt_zone_name" {
-  description = "Name of private zone"
-  type = string
+  default = " "
 }
 
 variable "enable_dns_support" {
-  type = bool
+  type    = bool
   default = true
 }
 
 variable "enable_dns_hostnames" {
-  type = bool
+  type    = bool
   default = true
 }
 
 variable "instance_tenancy" {
-  type = string
+  type    = string
   default = "default"
 }
 
 variable "log_destination_type" {
-  type = string
+  type    = string
   default = "s3"
 }
 
 variable "traffic_type" {
-  type = string
+  type    = string
   default = "ALL"
 }
 
 variable "enable_vpc_logs" {
-  type = bool
+  type    = bool
   default = false
 }
 
-variable "enable_alb_logging" {
-  type = bool
+variable "enable_igw" {
+  type    = bool
   default = true
 }
 
-variable "enable_deletion_protection" {
-  type = bool
-  default = false
+variable "igw_name" {
+  type        = string
+  description = "Internet Gateway name"
 }
