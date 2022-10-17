0f74a09c0a1c967ae54427f822ff5e14d0745fdf
Nikhil Panchal <nikhil.panchal@opstree.com>
2022-10-17 13:13:35 +0530

    updated output.tf file

--- output.tf
+++ output.tf
@@ -1,39 +1,16 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.main.*.id[0]
 }
 
-output "pub_route_table_id" {
-  description = "Public route table ID"
-  value = module.publicRouteTable.id
+output "vpc_arn" {
+  description = "The arn of the VPC"
+  value       = aws_vpc.main.*.arn[0]
 }
 
-output "pvt_route_table_id" {
-  description = "Private Route table ID" 
-  value = module.privateRouteTable.id
+output "igw_id" {
+  description = "The ID of the VPC"
+  value       = aws_internet_gateway.igw.*.id[0]
 }
 
-output "pub_alb_dns" {
-  value = module.pub_alb.dns_name
-}
 
-output "pvt_hosted_zone_id" {
-  description = "hosted zone id"
-  value       = aws_route53_zone.private_hosted_zone.zone_id
-}
-
-output "pvt_subnet_ids" {
-  value = module.PrivateSubnets.ids
-}
-
-output "public_subnet_ids" {
-  value = module.PublicSubnets.ids
-}
-
-output "private_subnet_ids" {
-  value = module.PrivateSubnets.ids
-}
-
-output "web_sg_id" {
-  value = module.public_web_security_group.sg_id
-}
